fc3288bd3576872ab44c44f58d7f9d6b41e2cf51
gun khemnak <gunnn_boy_hand@hotmail.com>
2020-12-16 22:42:02 +0700

    pcd

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -30,20 +30,20 @@ router.get('/closet-station', async (req: Request, res: Response) => {
       rs[0].LastUpdate.PM25.color = '#ffff02';
       rs[0].LastUpdate.PM25.level = 'ปานกลาง';
       rs[0].LastUpdate.PM25.volume = '(38 - 50 ไมโครกรัม/ลูกบาศก์เมตร)';
-      rs[0].LastUpdate.PM25.normalPeople = [];
-      rs[0].LastUpdate.PM25.sensitivePeople = [];
+      rs[0].LastUpdate.PM25.normalPeople = ['1.เลี่ยงการทํากิจกรรมหรือออกกําลังกายกลางแจ้ง', '2.เฝ้าระวังอาการ หากมีอาการผิดปกติให้รีบพบแพทย์ทันที'];
+      rs[0].LastUpdate.PM25.sensitivePeople = ['1.ลดการทํากิจกรรมหรือออกกําลังกายกลางแจ้ง', '2.หากต้องออกนอกอาคาร สวมหน้ากากอนามัยหรือ N95', '3.เฝ้าระวังอาการ หากมีอาการผิดปกติให้รีบพบแพทย์ทันที'];
     } else if (pm25Value <= 200) {
       rs[0].LastUpdate.PM25.color = '#ffc001';
       rs[0].LastUpdate.PM25.level = 'เริ่มมีผลกระทบต่อสุขภาพ';
       rs[0].LastUpdate.PM25.volume = '( 51 - 90 ไมโครกรัม/ลูกบาศก์เมตร)';
-      rs[0].LastUpdate.PM25.normalPeople = [];
-      rs[0].LastUpdate.PM25.sensitivePeople = [];
+      rs[0].LastUpdate.PM25.normalPeople = ['1.ลดการทํากิจกรรมหรือออกกําลังกายกลางแจ้ง', '2.หากต้องออกนอกอาคาร สวมหน้ากากอนามัยหรือ N95', '3.เฝ้าระวังอาการ หากมีอาการผิดปกติให้รีบพบแพทย์ทันที'];;
+      rs[0].LastUpdate.PM25.sensitivePeople = ['1.งดทํากิจกรรมหรือออกกําลังกายกลางแจ้ง', '2.สวมหน้ากากป้องกันฝุ่น และไม่อยู่นอกอาคารเป็นเวลานาน', '3.ผู้มีโรคประจําตัว เตรียมยาประจําตัวให้พร้อม'];
     } else if (pm25Value >= 201) {
       rs[0].LastUpdate.PM25.color = '#ff0000';
       rs[0].LastUpdate.PM25.level = 'มีผลกระทบต่อสุขภาพ';
       rs[0].LastUpdate.PM25.volume = '(91 ไมโครกรมั/ลูกบาศก์เมตร ขึ้นไป)';
-      rs[0].LastUpdate.PM25.normalPeople = [];
-      rs[0].LastUpdate.PM25.sensitivePeople = [];
+      rs[0].LastUpdate.PM25.normalPeople = ['1.งดทํากิจกรรมหรือออกกําลังกายกลางแจ้ง', '2.หากต้องออกนอกอาคาร สวมหน้ากากอนามัยหรือ N95', '3.ลดกิจกรรมทที่ทำให้เกิดฝุ่นในบ้าน เช่น กวาดบ้าน จุดธูป'];
+      rs[0].LastUpdate.PM25.sensitivePeople = ['1.งดออกนอกอาคาร ควรอยู่ในอาคารโดยเฉพาะห้องสะอาด', '2.หากต้องออกนอกอาคาร สวมหน้ากากอนามัยหรือ N95 ทกุครั้ง', '3.ผู้มีโรคประจําตัว เตรียมยาประจําตัวให้พร้อม'];
     }
     res.send({ ok: true, rows: rs });
   } catch (error) {
